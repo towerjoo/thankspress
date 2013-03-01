2584ebfa9c2c5fceec56551319877cbcb9339c9c
Dogukan Tufekci <dogukan@creco.co>
2013-03-01 04:11:17 +0000

    init

--- app/choices.py
+++ app/choices.py
@@ -1,5 +1,4 @@
 #coding: utf-8
-<<<<<<< HEAD
 
 class Choices(object):
     @classmethod
@@ -11,25 +10,3 @@ class Choices(object):
     @classmethod
     def from_string(cls, str):
         return getattr(cls, str.upper(), None)
-=======
-class Choices:
-    @classmethod
-    def get_value(cls, key):
-        # get the string display if need to show
-        for k, v in cls.CHOICES:
-            if k == key:
-                return v
-        return ""
-
-class CommentStatusChoices(Choices):
-    VISIBLE = 1
-    REPORTED = 2
-    DELETED = 3
-
-    CHOICES = (
-        (VISIBLE, "VISIBLE"),
-        (REPORTED, "REPORTED"),
-        (DELETED, "DELETED"),
-    )
-
->>>>>>> d5d7f110b65dfe96b3b227dd9cb4ba74b3d0acfd
--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 #!/venv/bin/python
-=======
-#!/usr/bin/env python
-import os
->>>>>>> d5d7f110b65dfe96b3b227dd9cb4ba74b3d0acfd
 import unittest
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     from app.user.test import TestCase as UserTestCase
     from app.thank.test import TestCase as ThankTestCase
     unittest.main(verbosity=2)
-=======
-    unittest.main()
->>>>>>> d5d7f110b65dfe96b3b227dd9cb4ba74b3d0acfd
